b890c64949b2cef429406075109b45208762ee39
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-12-30 01:41:44 +0800

    initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './assets/main.css'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+    },
+})
+app.mount('#app')
