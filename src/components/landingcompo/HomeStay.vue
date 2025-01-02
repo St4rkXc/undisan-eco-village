e80d8a64ba0c8e7996c5cf7f9896e3e9a12ab62a
Stark <123723458+St4rkXc@users.noreply.github.com>
2025-01-02 17:20:10 +0800

    new card component

---
+++ src/components/landingcompo/HomeStay.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineProps } from 'vue'
+
+
+defineProps({
+	img: {
+		type: String,
+		required: true,
+	},
+	title: {
+		type: String,
+		required: true,
+	},
+	price: {
+		type: String,
+		required: true,
+	},
+})
+</script>
+
+<template>
+	<img :src="img" alt="" class="w-full h-[300px] object-cover rounded-3xl" />
+	<div class="mt-4 -space-y-1">
+		<p class="text-text text-xl font-semibold">{{ title }}</p>
+		<p class="text-text/80 text-lg font-semibold">{{ price }}</p>
+	</div>
+</template>
